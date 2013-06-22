2ba0bf77d17d253c85ea4e1d36fed118323f8028
Madhusudhan Srinivasa <madhums8@gmail.com>
2013-06-22 14:31:08 +0530

    add passport config

---
+++ config/passport.js
@@ -0,0 +1,47 @@
+
+/*!
+ * Module dependencies.
+ */
+
+var mongoose = require('mongoose')
+var LocalStrategy = require('passport-local').Strategy
+var User = mongoose.model('User')
+
+/**
+ * Expose
+ */
+
+module.exports = function (passport, config) {
+  // serialize sessions
+  passport.serializeUser(function(user, done) {
+    done(null, user.id)
+  })
+
+  passport.deserializeUser(function(id, done) {
+    User.findOne({ _id: id }, function (err, user) {
+      done(err, user)
+    })
+  })
+
+  // use local strategy
+  passport.use(new LocalStrategy({
+      usernameField: 'email',
+      passwordField: 'password'
+    },
+    function(email, password, done) {
+      var options = {
+        criteria: { email: email }
+      }
+      User.load(options, function (err, user) {
+        if (err) { return done(err) }
+        if (!user) {
+          return done(null, false, { message: 'Unknown user' })
+        }
+        if (!user.authenticate(password)) {
+          return done(null, false, { message: 'Invalid password' })
+        }
+        return done(null, user)
+      })
+    }
+  ))
+}
