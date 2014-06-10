e7d53e5fbb51bb462caf199fa7da1f356c4e8e23
Madhusudhan Srinivasa <madhums8@gmail.com>
2014-06-11 00:10:51 +0200

    fix config

--- config/config.js
+++ config/config.js
@@ -5,15 +5,21 @@
 
 var path = require('path');
 var extend = require('util')._extend;
+
 var development = require('./env/development');
 var test = require('./env/test');
 var production = require('./env/production');
+
 var defaults = {
   root: path.normalize(__dirname + '/..')
 };
 
+/**
+ * Expose
+ */
+
 module.exports = {
-  development: extend(defaults, development),
-  test: extend(defaults, test),
-  production: extend(defaults, production)
+  development: extend(development, defaults),
+  test: extend(test, defaults),
+  production: extend(production, defaults)
 }[process.env.NODE_ENV || 'development'];
