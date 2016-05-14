044bf7d77e271c74a944af8f7daf8517aad61812
Madhusudhan Srinivasa <madhums8@gmail.com>
2016-05-14 16:23:38 +0200

    fill up dummy test

--- test/test-home.js
+++ test/test-home.js
@@ -8,6 +8,11 @@ const test = require('tape');
 const request = require('supertest');
 const app = require('../server');
 
-test('Some test', t => {
-  t.end();
+test('Home page', t => {
+  request(app)
+    .get('/')
+    .expect(200)
+    .end(t.end);
 });
+
+test.onFinish(() => process.exit(0));
