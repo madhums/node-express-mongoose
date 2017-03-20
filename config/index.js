b0fca610ccf04ef39c3969174b6647100c616c20
Madhusudhan Srinivasa <madhums8@gmail.com>
2017-03-20 22:57:23 +0100

    replace util._extend with Object.assign

--- config/index.js
+++ config/index.js
@@ -4,12 +4,9 @@
  */
 
 var path = require('path');
-var extend = require('util')._extend;
-
 var development = require('./env/development');
 var test = require('./env/test');
 var production = require('./env/production');
-
 var defaults = {
   root: path.normalize(__dirname + '/..')
 };
@@ -19,7 +16,7 @@ var defaults = {
  */
 
 module.exports = {
-  development: extend(development, defaults),
-  test: extend(test, defaults),
-  production: extend(production, defaults)
+  development: Object.assign({}, development, defaults),
+  test: Object.assign({}, test, defaults),
+  production: Object.assign({}, production, defaults)
 }[process.env.NODE_ENV || 'development'];
