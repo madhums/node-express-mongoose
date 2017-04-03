61b69783596062b7465593b0469a030224c877b4
Nontapat <astider@gmail.com>
2017-04-03 17:02:42 +0700

    try to understanding promise

--- server.js
+++ server.js
@@ -135,27 +135,13 @@ let quiz = nodeSchedule.scheduleJob('1 30 9 * * *', function(){
 async function prepareQuiz() {
 
   try {
-
-    let a = await database.ref('quiz').once('value')/*.then(function(snapshot){
-      let quizObject = snapshot.val()
-      console.log('quiz here');
-      console.log(quizObject);
-      console.log(quizObject.length);
-      for(let i = 0; i < quizObject.length; i++) {
-        console.log(quizObject[i].q);
-        console.log(`choices: ${quizObject[i][0]}, ${quizObject[i][1]}`);
-      }
-
-      return quizObject
-    })
-    */
+    let a = await database.ref('quiz').once('value')
     return a.val()
   }
   catch(error) {
     console.log('get quiz error');
     console.log(error);
   }
-
 
 }
 
@@ -194,6 +180,9 @@ userMgt.getAllSubscribedID(function(err, ids){
 })
 
 let quizPromise = Promise.resolve(prepareQuiz())
+
+console.log(quizPromise);
+console.log(JSON.stringify(quizPromise));
 
 quizPromise.then((quiz) => {
   console.log('chekc size');
