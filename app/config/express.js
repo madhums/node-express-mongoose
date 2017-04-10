bafac3ce662fa6ebc13973ef36b71942fc314e1e
Nontapat <astider@gmail.com>
2017-04-10 18:50:41 +0700

    set view engine

--- app/config/express.js
+++ app/config/express.js
@@ -3,6 +3,10 @@ let quizResult = require('../controllers/quizResult.controller.js')
 
 module.exports = function(app) {
 
+  app.set('views', config.root + '../views');
+  app.set('view engine', 'ejs');
+
   app.get("/result", quizResult.getResult)
+
 
 }
