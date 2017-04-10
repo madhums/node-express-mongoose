680fbedcb2eb4491011f772193aacb42b39531be
Nontapat <astider@gmail.com>
2017-04-10 15:58:49 +0700

    module.exports

--- express.js
+++ express.js
@@ -5,10 +5,10 @@ let messengerProfileAPI = require('./app/apis/messenger_profile.api.js')
 let userMgt = require('./app/controllers/userManagement.controller.js')
 let database = userMgt.database
 
-//module.exports = function() {
+module.exports = function() {
 
   app.get("/test", function(req, res){
     res.send('sss')
   })
 
-//}
+}
