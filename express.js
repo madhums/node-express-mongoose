547a2024cc8529e79bae75e5f4d8ba345d9210bf
Nontapat <astider@gmail.com>
2017-04-10 17:00:20 +0700

    put app = express() of express.js into module.exports

--- express.js
+++ express.js
@@ -1,14 +1,16 @@
 let express = require('express')
-let app = express()
+
 
 let messengerProfileAPI = require('./app/apis/messenger_profile.api.js')
 let userMgt = require('./app/controllers/userManagement.controller.js')
 let database = userMgt.database
 
-app.get("/test", function(req, res){
-  res.send('sss')
-})
-
 module.exports = function() {
-  app: app
+
+  let app = express()
+
+  app.get("/test", function(req, res){
+    res.send('sss')
+  })
+
 }
