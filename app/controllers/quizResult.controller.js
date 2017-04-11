bbaa057ca8208bb7726e7b25ae72d75245747bf6
Nontapat <astider@gmail.com>
2017-04-11 17:46:35 +0700

    what is the type of usersChunk

--- app/controllers/quizResult.controller.js
+++ app/controllers/quizResult.controller.js
@@ -52,7 +52,7 @@ exports.getResult = function(req, res) {
       })
       */
       console.log('\n\n\n');
-      console.log(usersChunk[0]);
+      console.log(usersChunk);
     }
 
     result.sort( (a,b)=> { return (a.point > b.point) ? 1 : ( (b.point > a.point) ? -1 : 0 ) })
