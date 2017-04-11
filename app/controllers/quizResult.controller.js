0e3cea719d1ca80a9ca3b6e116d1d066a7917e00
Nontapat <astider@gmail.com>
2017-04-11 15:40:15 +0700

    test render

--- app/controllers/quizResult.controller.js
+++ app/controllers/quizResult.controller.js
@@ -16,31 +16,27 @@ exports.getResult = function(req, res) {
 
   })
   .then((quizSnapshot)=>{
-    let quiz = quizSnapshot.val()
-    console.log(quiz);
-    res.send(`${quiz}`)
 
+    let quiz = quizSnapshot.val()
     let allCorrectUsers = []
+
     allCorrectUsers = quiz.map((q)=>{
       return q.correctUsers
     })
 
     allCorrectUsers.forEach((userByOrder)=>{
       userByOrder.forEach((uid)=>{
-        console.log(JSON.stringify(uid));
-
         if(result.hasOwnProperty(uid))
           result[uid]++
-
       })
     })
 
     console.log(`end result: ${JSON.stringify(result)}`);
 
   })
-  /*
+
   res.render("result", {
-    a: a
+    a: result
   })
-  */
+
 }
