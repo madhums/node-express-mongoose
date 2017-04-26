297099a6a21a282417c723d8ce9a5a4db221db0c
Nontapat <astider@gmail.com>
2017-04-26 17:43:25 +0700

    remove duplicate section causing error

--- server.js
+++ server.js
@@ -459,47 +459,7 @@ function startQuiz() {
 }
 
 //let quiz = nodeSchedule.scheduleJob('0 30 9 * * *', function(){
-  quizPromise.then((quiz) => {
 
-    ttq = quiz
-    quizReady = new Array(ttq.length).fill(false)
-    console.log(`quizready = ${quizReady}`);
-
-    userMgt.getAllID(function(err, list){
-      if(err) console.log(err);
-      else if(list) {
-        allIDs = list
-        enterTime = true
-
-        console.log('parti : ' + allIDs);
-
-        allIDs.map((id)=>{
-          messengerBot.sendTextMessageTo('กิจกรรมกำลังจะเริ่มในไม่ช้า', id)
-          setTimeout(()=>{
-            messengerBot.sendDefaultButtonMessageTo(['เข้าร่วม', 'ไม่เข้าร่วม'], id, 'ผู้สนใจสามารถกดเข้าร่วมได้ตามปุ่มด้านล่างนี้เลย');
-          }, 500)
-        })
-
-        console.log('CLOCK STARTED');
-
-        setTimeout(()=>{
-          console.log('ALLID: ' + allIDs);
-          console.log('P_ID: ' + participants);
-          enterTime = false
-
-          if(participants.length > 0) startQuizTime(quiz, participants)
-          else {
-            allIDs.map((id)=>{
-              messengerBot.sendTextMessageTo('เสียใจ ไม่มีใครเล่นด้วยเลย :(', id)
-            })
-            console.log('no one want to play quiz');
-          }
-
-        }, 30000) //300000
-
-      }
-    })
-  })
 //})
 
 
