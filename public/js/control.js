f5e6f4ba5ddbe80177a4dcc824257cf0458a50df
Nontapat <astider@gmail.com>
2017-04-26 18:42:27 +0700

    updateStatus now update quizReady status

--- public/js/control.js
+++ public/js/control.js
@@ -30,16 +30,49 @@ function updateStatus() {
   .done((data)=>{
 
     console.log(data);
-    //$("#ready").html("YES")
-    if(data.readyToStart) $("#ready").html("YES")
-    else $("#ready").html("NO")
 
-    if(data.enterTime) $("#enterStatus").html("OPEN")
-    else $("#enterStatus").html("CLOSE")
+    // ready
+    if(data.readyToStart) {
+      $("#ready").html("YES")
+      $("#ready").css('color', '#00ff00')
+    }
+    else {
+      $("#ready").html("NO")
+      $("#ready").css('color', 'red')
+    }
 
-    if(data.isQuizOnline) $("#quizOnlineStatus").html("ONLINE")
-    else $("#quizOnlineStatus").html("OFFLINE")
+    //enter status
+    if(data.enterTime) {
+      $("#enterStatus").html("OPEN")
+      $("#enterStatus").css('color', '#00ff00')
+    }
+    else {
+      $("#enterStatus").html("CLOSE")
+      $("#enterStatus").css('color', 'red')
+    }
 
+    //isQuizOnline
+    if(data.isQuizOnline) {
+      $("#quizOnlineStatus").html("ONLINE")
+      $("#quizOnlineStatus").css('color', '#00ff00')
+    }
+    else {
+      $("#quizOnlineStatus").html("OFFLINE")
+      $("#quizOnlineStatus").css('color', 'red')
+    }
+
+    //quizReadyStatus
+    if(quizReady) {
+      quizReady.forEach((quiz)=>{
+        let elem = ""
+
+        if(quiz) elem = `<span id=\`q${index+1}Status\` style="color: #00ff00"><strong>false</strong></span>`
+        else elem = `<span id=\`q${index+1}Status\` style="color: #ff0000"><strong>false</strong></span>`
+
+        $("#quizReadyStatus").append(elem)
+
+      })
+    }
 
   })
   .fail((error)=>{
