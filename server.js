3698cc490450cc838ed253fd767c2939b2ad15b3
Nontapat <astider@gmail.com>
2017-05-01 17:48:03 +0700

    fix variable scope error

--- server.js
+++ server.js
@@ -119,7 +119,7 @@ botmaster.on('update', (bot, update) => {
       console.log('nowP: '+ participants);
 
       if(update.message.text != "เข้าร่วม" && participants.indexOf(update.sender.id) < 0) {
-        messengerBot.sendDefaultButtonMessageTo(['เข้าร่วม', 'ไม่เข้าร่วม'], id, 'สนใจเล่นกิจกรรมกับเราใช่มั้ย กดเข้าร่วมได้ตามปุ่มด้านล่างนี้เลย');
+        messengerBot.sendDefaultButtonMessageTo(['เข้าร่วม', 'ไม่เข้าร่วม'], update.sender.id, 'สนใจเล่นกิจกรรมกับเราใช่มั้ย กดเข้าร่วมได้ตามปุ่มด้านล่างนี้เลย');
       }
       else {
 
